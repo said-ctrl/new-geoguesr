7efce023c88cc3a5b37c4ed2e6f7ce47109e135d
said-ctrl <fifinabs@gmail.com>
2024-09-03 16:37:13 +0200

    ede

--- end/new.js
+++ end/new.js
@@ -1,10 +1,6 @@
-var marker;
-map.on('click', function(e) {
-    if (marker) {
-        map.removeLayer(marker);
-    }
-    marker = L.marker(e.latlng).addTo(map);
-    // Stocke la position devinée par le joueur
-    var guessedLocation = e.latlng;
-    console.log('Position devinée :', guessedLocation);
-});
+let maps = L.map('maps').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(maps);
+
--- end/style.js
+++ end/style.js
@@ -16,7 +16,7 @@ let popup = L.popup();
 
 
 map.on('click', (e) => {
-  
+
     popup
         .setLatLng(e.latlng)
         .setContent("You clicked the map at " + e.latlng.toString())
@@ -24,8 +24,9 @@ map.on('click', (e) => {
     let lat = e.latlng.lat;
     let lng = e.latlng.lng;
     pos = [lat, lng];
-  
-    
+
+    ;
+
 });
 
 
@@ -42,32 +43,157 @@ map.setView(randomLocation, 1);
 
 
 
+
 var marker = L.marker(randomLocation).addTo(map);
 
-console.log(randomLocation);
 
 // Récupérer le bouton par son ID
 var button = document.getElementById('button');
 
 // Ajouter un événement 'click' au bouton
-button.addEventListener('click', function () { var marker
-  
-  console.log(pos);
-  
-    
+button.addEventListener('click', function () {
+    button.style.display = "none";
+
+
+
+
+
 });
 
 // Récupérer la position sauvegardée au chargement de la page
-window.onload = function () {
-    var savedPosition = localStorage.getItem('clickPosition');
-    if (savedPosition) {
-        var position = JSON.parse(savedPosition);
-        L.marker([position.lat, position.lng]).addTo(map);
-        map.setView([position.lat, position.lng], 13);
-    }
-};
-function calculateScore(randomLocation, guessedLocation) {
-    const distance = map.distance(actualLocation, guessedLocation);
-    return Math.max(0, 5000 - distance / 1000); // Exemple de calcul de score
 
-}
+var resultat = document.getElementById('btn');
+
+//appel du bouton et cacher le bouton//
+resultat.addEventListener('click', function () {
+
+    setTimeout(() => {
+        resultat.style.display = "none";
+    }, 500);
+
+
+
+
+
+
+
+    var latitude = pos[0] - randomLocation[0];
+    var longitude = pos[1] - randomLocation[1];
+
+    var double = [randomLocation, pos];
+    //cree la ligne de distance
+    var polyline = L.polyline(double, { color: 'red' }).addTo(map);
+
+    var distance = map.distance(pos, randomLocation);
+
+
+
+
+    document.getElementById('result').textContent = "La distance entre les deux points est de " + distance + " metre.";
+
+
+});
+let data = [
+    {
+        "image": ["images/maroc.jpg"],
+        "lati": 47.556225,
+        "longi": 13.645990,
+
+
+    },
+    {
+        "image": ["images/montbe.webp"],
+        "lati": 24.183438,
+        "longi": 102.230346,
+
+    },
+    {
+        "image": ["images/paris.jpg"],
+        "lati": -22.980226,
+        "longi": -43.189026,
+    },
+    {
+        "image": ["images/qatar.webp"],
+        "lati": 4.333397,
+        "longi": 73.599140,
+
+    },
+    {
+        "image": ["images/russie.jpg"],
+        "lati": 45.553261,
+        "longi": -73.581531,
+
+    },
+    {
+        "image": ["images/sarrebourg.jpg"],
+        "lati": 55.758390,
+        "longi": 37.635335,
+
+    },
+    {
+        "image": ["images/ukraine.jpg"],
+        "lati": 48.070569,
+        "longi": 6.855323,
+
+    },
+
+    {
+        "image": ["images/japon.jpg"],
+        "lati": 47.556225,
+        "longi": 13.645990,
+
+
+    },
+    {
+        "image": ["images/ilescanarie.jpg"],
+        "lati": 47.556225,
+        "longi": 13.645990,
+
+    },
+    {
+        "image": ["images/dubai.avif"],
+        "lati": 25.2653471,
+        "longi": 55.2924914,
+    },
+    {
+        "image": ["images/chine.jpg"],
+        "lati": 35.000074,
+        "longi": 104.999927,
+    },
+    {
+        "image": ["images/brazil.jpg"],
+        "lati": -14.235004,
+        "longi": -51.92528,
+    },
+    {
+        "image": ["images/allemagne.jpg"],
+        "lati": 51.165707,
+        "longi": 10.452764,
+    },
+    {
+        "image": ["images/afriquesud.jpg"],
+        "lati": -28.48322,
+        "longi": 24.676997,
+
+    },
+
+
+]
+// fonction choix aleatoirs//
+function getRandomItem(arr) {
+
+    return randomIndex = Math.floor(Math.random() * data.length);
+
+}let result = getRandomItem(data);
+console.log(result )
+
+let phot = document.getElementById('photo');
+let photo = document.createElement('img');
+
+photo.style.height = "25rem";
+photo.src = data[result]['image'];
+phot.appendChild(photo);
+
+
+
+
